2e45ffb32aec9e3b773bfa87349e0f7a914d99f0
jdboom <jdboomg@ilstu.edu>
2017-11-13 20:18:28 -0600

    Update RecipeBook.java

--- src/edu/ilstu/RecipeBook.java
+++ src/edu/ilstu/RecipeBook.java
@@ -4,49 +4,60 @@ import java.util.ArrayList;
 
 public class RecipeBook
 {
-	
+	private static RecipeBook obj;
 	private ArrayList<Recipe> recipeList;
-	
+
+	private RecipeBook()
+	{
+		recipeList = new ArrayList<Recipe>();
+	}
+
+	public static RecipeBook getInstance()
+	{ if (obj == null)
+		{obj = new RecipeBook();}
+	return obj;
+	}
+
 	RecipeBook(Recipe r)
 	{
-		recipeList = new ArrayList<Recipe>();
+		recipeList = RecipeBook.getInstance().listRecipes();
 		recipeList.add(r);
 	}
-	
-	RecipeBook()
+
+	public ArrayList<Recipe> listRecipes()
 	{
-		recipeList = new ArrayList<Recipe>();
+		return this.recipeList;
 	}
-	
+
 	public void addRecipe(Recipe newRecipe)
 	{
 		recipeList.add(newRecipe);
 	}
-	
+
 	public void deleteRecipe(Recipe oldRecipe)
 	{
 		recipeList.remove(oldRecipe);
 	}
-	
+
 	public void sort()
 	{
 		//Sort with different parameters
 	}
-	
+
 	public boolean save()
 	{
 		return true;
 		//Need database info first
 	}
-	
+
 	public void load()
 	{
 		//Need database info first
 	}
-	
+
 	public void displayAllRecipes()
 	{
 		//Need UI info
 	}
-	
+
 }
