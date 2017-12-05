679b78caad4cc3e6e77f054adfb7ca0712b4a48a
jdboom <jdboomg@ilstu.edu>
2017-12-04 19:59:51 -0600

    Update cStockWindow.java

--- src/edu/ilstu/cStockWindow.java
+++ src/edu/ilstu/cStockWindow.java
@@ -1,4 +1,5 @@
-* To change this license header, choose License Headers in Project Properties.
+/*
+ * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -185,7 +186,7 @@ private Ingredient tempIngredient = null;
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void mainTemp(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
